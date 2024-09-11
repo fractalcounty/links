7c687a6abf83104e0a87c1829ffdb9cff47933d8
fractalcounty <chip@fractalcounty.com>
2024-09-11 08:55:11 -0500

    Added editor support for Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
+    editorSupport: { autocompleteUtil: { as: 'tailwindClasses' } },
     config: {
       plugins: [require('daisyui')],
       daisyui: {
