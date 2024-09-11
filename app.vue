ec36f922826a395ec9ab2fb47a7adff85233281e
fractalcounty <chip@fractalcounty.com>
2024-09-11 10:01:50 -0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,12 +3,3 @@
     <h1>Hello world</h1>
   </div>
 </template>
-
-<style lang="postcss" scoped>
-  .badge {
-    @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
-    &:hover {
-      @apply bg-gray-300;
-    }
-  }
-</style>
