5009d29e2b0d9d765a2d98f52a9e692cec3c1a16
fractalcounty <chip@fractalcounty.com>
2024-09-11 12:15:03 -0500

    Added `@nuxt/image`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/image'],
   tailwindcss: {
     editorSupport: { autocompleteUtil: { as: 'tailwindClasses' } },
+  },
+  image: {
+    format: ['avif', 'webp'],
   },
   fonts: {
     families: [
